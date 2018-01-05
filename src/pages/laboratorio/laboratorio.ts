a3db494fe445098d2348049e94360b2f3d898834
xGoldwarx <goldwar01@hotmail.com>
2018-01-05 18:35:09 -0500

    laboratorio json

--- src/pages/laboratorio/laboratorio.ts
+++ src/pages/laboratorio/laboratorio.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 /**
  * Generated class for the LaboratorioPage page.
  *
@@ -14,8 +15,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'laboratorio.html',
 })
 export class LaboratorioPage {
+  public userID:number;
+  public list: any;
+  newsData:any;
+  loading:any;
+  posts = [];
+  constructor(private http:Http, public navCtrl: NavController, public navParams: NavParams) {
+    this.userID=this.navParams.get('userID');
+    console.log(this.userID);
+    /**MOSTRAR GET LIST DE PLATOS.TS 
+    users.getListPlato().then(results=>{
+      console.log(results);
+      this.list=results;
+    }).catch(err=>{
+      console.log(err);
+    });
+   */
+          this.http.get('../../assets/data/disponibilidad.json').map(res => res.json())
+          .subscribe(data=>{
+            this.list=data;
+            console.log('Los datos son: ' ,data);
+          },
+          err =>{
+            console.error("Error : "+err);
+          } ,
+          () => {
+            console.log('laboratorios completados'+ this.list);
+          }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+        );
+  
+    /*this.getdata();*/
   }
 
   ionViewDidLoad() {
